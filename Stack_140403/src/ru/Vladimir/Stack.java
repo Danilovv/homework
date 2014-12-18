ad1b09b55ad9d3a185d522edb0ba43e171082fa0
Vladimir <vladimir_danilov@epam.com>
2014-12-18 20:08:44 +0300

    Add readme

---
+++ Stack_140403/src/ru/Vladimir/Stack.java
@@ -0,0 +1,35 @@
+package ru.Vladimir;
+
+/**
+ * Created by Vladimir_Danilov on 18-Dec-14.
+ */
+public class Stack {
+
+    Object[] items;
+    private int index = 0;
+
+    public Stack(int maxSize) {
+        items = new Object[maxSize];
+    }
+
+    public void push(Object item) {
+        items[index++] = item;
+    }
+
+    public Object pop() {
+        return items[--index];
+    }
+
+    public Object top() {
+        return items[index-1];
+    }
+
+    public boolean isEmpty() {
+        return getSize() == 0;
+    }
+
+    public int getSize() {
+        return index;
+    }
+
+}
