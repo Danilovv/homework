10054d1df7dd5e22c7e19cc50a958ff320fcb8e0
Vladimir <vladimir_danilov@epam.com>
2014-12-18 18:40:32 +0300

    A little bit better.

--- classwork/src/ru/Vladimir/A.java
+++ classwork/src/ru/Vladimir/A.java
@@ -3,14 +3,10 @@ package ru.Vladimir;
 /**
  * Created by Vladimir_Danilov on 18-Dec-14.
  */
-public class A extends X implements I {
+public class A extends X {
 
     @Override
     public void doIt() {
         System.out.println("I did it by myself");
-    }
-
-    public I getI() {
-        return this;
     }
 }
